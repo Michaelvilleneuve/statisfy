0a987deca2ff0075fc9e457f168a16e72b24438b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-12-11 13:10:54 +0100

    Better way of handling model based counters

--- lib/statisfy/model.rb
+++ lib/statisfy/model.rb
@@ -11,24 +11,25 @@ module Statisfy
 
     module ClassMethods
       def count(params)
-        a_bloc = params.fetch(:scopes, nil) || params.fetch(:uniq_by, nil) || params.fetch(:if, nil)
-        source = a_bloc&.source
+        raise ArgumentError, "Missing :as parameter" unless params[:as]
 
         class_name = params[:as].to_s.camelize
 
         eval <<-RUBY, binding, __FILE__, __LINE__ + 1
           class ::Statisfy::#{class_name}
             include Statisfy::Counter
-
-            #{source || "count(#{params})"}
           end
         RUBY
+
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def self.#{params[:as]}
             ::Statisfy::#{class_name}
           end
         RUBY
+
+        "::Statisfy::#{class_name}".constantize.send(__method__, **params)
       end
+      alias aggregate count
     end
   end
 end
